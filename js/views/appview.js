43545b2f9ea27fda963b98013729b9e2ce82c882
borrik <borrik@live.ru>
2018-11-04 16:07:09 +0200

    model-behave change

--- js/views/appview.js
+++ js/views/appview.js
@@ -11,13 +11,7 @@ var app = app || {};
             this.listenTo(app.Netc, 'add', this.addOne);
             this.listenTo(app.Netc, 'reset', this.addAll);
             app.Netc.fetch();
-
-        //adding network view element
-        // this.$el.append(new app.ItemsView().el);
-        // listen to model changes
-
         },
-
 
         ///rendering newly arrived elements (applying to DOM one-by-one)
         addOne: function( elem ) {
@@ -43,91 +37,20 @@ var app = app || {};
 (function () {
     app.ItemsView = Backbone.View.extend({
         netTmpl: _.template($('#item-template').html()),
-        //renderCounter:0,
 
-        // events: {
-        //     'click .editButton': function (ev) {
-        //         let id=ev.target.id;
-        //         this.$el.find(`.toggled[id="${id}"]`).toggle('slow');
-        //         //this.$el.find('.toggle')
-        //     },
-        //     "click .destroy": function (ev) {
-        //         let id=$(ev.target).attr('id');
-        //         //do whatever you want with id
-        //         this.removeFromAll(id)
-        //         app.oRouter.navigate("")
-        //     },
-        //     "click .refresh": function (ev) {
-        //         //partial update?
-        //         let id=$(ev.target).attr('id')
-        //     },
-        //     "click .save": function (ev) {
-        //         let id=parseInt($(ev.target).attr('id'));
-        //         let model=app.netcollect.get(id);
-        //         model.save();
-        //         //when typing - store text  in local model id  and save it
-        //         // on server when sace is pressed
-        //     },
-        //     "focus .edit": function(ev){
-        //         app.oRouter.navigate("edit/"+ev.target.id)
-        //
-        //     },
-        //
-        //     "keyup .edit": function (ev) {
-        //         let id=parseInt($(ev.target).attr('id'));
-        //         let text=$(ev.target).val();
-        //         app.netcollect.set({id:id, nettitle: text})
-        //     },
-        //     //update url in case of input field and reset in case of other element
-
-       // },
 //one initialization for all models ->>> own init for each model
         tagName: 'li',
         initialize:  function(){
             console.log('ItemsView view initialized');
             this.listenTo(this.model, 'change', this.render);
-            //this.$button=this.$('button');
-
-
-            // this.fetchContent().then(()=>{
-            // this.render()
-            //  });
-
        },
         //on-demand rendering
         render: function() {
             console.log('rendering ',this.model.attributes );
             this.$el.html(this.netTmpl(this.model.attributes));
             return this;
-            //
-            // this.$el.html('');
-            // //sorting:
-            // this.sorted = app.netcollect.sortBy(function (el) {
-            //     return el.get('id')
-            // });
-            // this.sorted.forEach(netTemplateModel=> {
-            // this.$el.append(this.netTmpl(netTemplateModel.attributes));
-            // this.$el.find('.toggled').hide();
-            // this.$el.find('button').css('background-color', 'gold');
-            // this.$el.find('div').css('background-color', 'beige');
-            // })
         },
-        // fetchContent:  function (id) {
-        //     //  await
-        //     return new Promise(resolve =>
-        //         {
-        //     resolve(app.netcollect.fetch({
-        //         type: 'GET',
-        //         success: function(collection, response) {
-        //             _.each(collection.models, function(model) {
-        //             })
-        //         },
-        //         error: function (resp) {
-        //             console.log('error fetching',arguments)
-        //         }
-        //     }));
-        //
-        // })},
+
 
         removeFromAll: function(id){
            this.tobedeleted = app.netcollect.get(id);
